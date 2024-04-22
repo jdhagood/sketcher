a75e5f3e5a62697c4e03e3a46794df710b451e06
Sketcher Pi <jdhagood104@gmail.com>
2024-04-21 23:53:58 -0400

    This is a test commit message

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ Main python script for the robotic etch a sketch
 run this in the sketcher virbutal enviroment ($ source sketcher/bin/activate)
 """
 
-print("Hello world") v
+print("Hello world")
+
+#this is a test
